d09e7482f8ceb3f813aba08b80b749bfabb345b2
Itai Roded <itai.roded@gmail.com>
2025-08-13 13:33:35 -0500

    WIP

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-"# JavaItaiPlayground" 
+"# JavaItaiPlayground"
+
+FULL ITAI NOTES LINKS!!
+https://docs.google.com/document/d/1BPge5MPJc3PACIKplfmhFXEq5XYbjwX6aa2NSSaWiLU/edit?usp=sharing
+https://docs.google.com/document/d/1BPge5MPJc3PACIKplfmhFXEq5XYbjwX6aa2NSSaWiLU/edit?usp=sharing
--- src/main/java/com/itai/itaiJavaPlay/ItaiJavaPlayApplication.java
+++ src/main/java/com/itai/itaiJavaPlay/ItaiJavaPlayApplication.java
@@ -2,12 +2,22 @@ package com.itai.itaiJavaPlay;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class ItaiJavaPlayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ItaiJavaPlayApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ItaiJavaPlayApplication.class, args);
+    }
 
+    @RestController
+    public static class HealthController {
+        @GetMapping("/health")
+        public String health() {
+            return "OK - Itai's Java Play is running perfectly!";
+        }
+    }
 }
+
