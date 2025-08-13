5fb5a93a52d3db881f7ad453a3c59e82045fd6e8
Itai Roded <itai.roded@gmail.com>
2025-08-13 13:24:54 -0500

    spring starter

---
+++ src/main/java/com/itai/itaiJavaPlay/ItaiJavaPlayApplication.java
@@ -0,0 +1,13 @@
+package com.itai.itaiJavaPlay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItaiJavaPlayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItaiJavaPlayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itai/itaiJavaPlay/ItaiJavaPlayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itai.itaiJavaPlay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItaiJavaPlayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
